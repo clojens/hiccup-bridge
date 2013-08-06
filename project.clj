418e7160fd937efae187432ca91d5a86ef7bf06f
supersymmetry <maildumpert@gmail.com>
2013-08-06 02:06:33 +0200

    updated dep vers + git-flow init

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject hiccup-bridge "1.0.0"
   :description "Hiccup to html, html to hiccup. Fork with updated deps for 1.5.x branch."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.hozumi/clj-glob "0.1.2"]
+                 [clj-glob "1.0.0"]
                  [clj-det-enc "1.0.0"]
-                 [hiccup "1.0.3"]
+                 [hiccup "1.0.4"]
                  [enlive "1.1.1"]])
